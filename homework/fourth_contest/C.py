9ddc3fbb42c08567e3c95ae2ba949d6d476288ef
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-03-21 21:23:21 +0500

    add the third tast of the fourth contest

---
+++ homework/fourth_contest/C.py
@@ -0,0 +1,34 @@
+def enough_distance_between_cows(min_distance, positions, num_of_cows):
+    cows_placed = 1  # Первая корова размещена на позиции positions[0]
+    last_cow_position = positions[0]
+
+    # Перебираем позиции коров в порядке их расположения
+    for position in positions:
+        if position - last_cow_position >= min_distance:
+            cows_placed += 1
+            last_cow_position = position
+
+    # Проверяем, достаточно ли размещено коров
+    return cows_placed >= num_of_cows
+
+
+def get_maximum_min_distance(positions, num_of_cows):
+    left = 0
+    right = positions[-1] - positions[0] + 1
+
+    while right - left != 1:
+        mid = (left + right) // 2
+        # Если можно разместить коров на расстоянии mid, увеличиваем min_distance
+        if enough_distance_between_cows(mid, positions, num_of_cows):
+            left = mid
+        # Иначе уменьшаем min_distance
+        else:
+            right = mid
+
+    return left
+
+
+n, k = map(int, input().split())
+cow_positions = list(map(int, input().split()))
+
+print(get_maximum_min_distance(cow_positions, k))
